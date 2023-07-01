70c41f8244ac134939dc49873af1f52f9eb62174
Aslam Ahammed A <aslamplr@gmail.com>
2023-07-01 13:56:17 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// use serde::{Deserialize, Serialize};
+// use surrealdb::engine::local::File;
+// use surrealdb::Surreal;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // let db = Surreal::new::<File>("/tmp/cgpt.rocks.db").await?;
+
+    println!("Hello, world!");
+
+    Ok(())
+}
