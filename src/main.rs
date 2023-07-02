062faffb1b9ce2cebdd9d066022145df55c5f6ac
Aslam Ahammed A <aslamplr@gmail.com>
2023-07-02 15:15:51 +0530

    Update with config and chat cached locally

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,59 @@
-// use serde::{Deserialize, Serialize};
-// use surrealdb::engine::local::File;
-// use surrealdb::Surreal;
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::local::File;
+use surrealdb::Surreal;
 
 use std::io::Write;
 
 use anyhow::Context;
 use async_openai::{
-    types::{ChatCompletionRequestMessageArgs, CreateChatCompletionRequestArgs, Role},
+    types::{
+        ChatCompletionRequestMessage, ChatCompletionRequestMessageArgs,
+        CreateChatCompletionRequestArgs, Role,
+    },
     Client,
 };
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Chat {
+    messages: Vec<ChatCompletionRequestMessage>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    if !std::env::var("OPENAI_API_KEY").is_ok() {
-        dotenv::dotenv()
-            .with_context(|| "Set OPENAI_API_KEY environment variable or add in .env file")?;
-    }
+    let db_path = if let Some(config) = config::get_config().await {
+        std::env::set_var("OPENAI_API_KEY", config.openai_api_key.as_ref());
+        config.surreal_db_path
+    } else {
+        if std::env::var("OPENAI_API_KEY").is_err() {
+            dotenv::dotenv()
+                .with_context(|| "Set OPENAI_API_KEY environment variable or add in .env file")?;
+        }
+        config::save_config(config::Config::new(
+            std::env::var("OPENAI_API_KEY").unwrap_or_default(),
+            "/tmp/cgpt.cli.rocks.db",
+        ))
+        .await?;
+        "/tmp/cgpt.cli.rocks.db".to_string().into()
+    };
+    let db_path = db_path.as_ref();
 
     let openai_client = Client::new();
-    // let db = Surreal::new::<File>("/tmp/cgpt.rocks.db").await?;
+    let db = Surreal::new::<File>(db_path).await?;
 
-    let mut messages = vec![ChatCompletionRequestMessageArgs::default()
-        .role(Role::System)
-        .content(
-            "You are a helpful software engineer expert in Rust language and AWS Cloud Platform.",
-        )
-        .build()?];
+    db.use_ns("cgpt").use_db("default").await?;
+
+    let chat: Option<Chat> = db.select(("chat", "default_chat")).await?;
+
+    let mut messages = if let Some(chat) = chat {
+        chat.messages
+    } else {
+        vec![ChatCompletionRequestMessageArgs::default()
+            .role(Role::System)
+            .content(
+                "You are a helpful software engineer expert in Rust language and AWS Cloud Platform.",
+            )
+            .build()?]
+    };
 
     println!("This is a chat gpt CLI; type `exit` or Control-C to exit the promt! Requires internet conntectivity!");
 
@@ -65,7 +93,7 @@ async fn main() -> anyhow::Result<()> {
                     .message
                     .content
                     .as_deref()
-                    .unwrap_or_else(|| "No content in response!")
+                    .unwrap_or("No content in response!")
             );
         } else {
             println!("No response!")
@@ -73,4 +101,65 @@ async fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+mod config {
+    use std::borrow::Cow;
+
+    use anyhow::Result;
+    use serde::{Deserialize, Serialize};
+    use std::path::PathBuf;
+    use tokio::fs::{self, File};
+    use tokio::io::AsyncWriteExt;
+    use tokio::task;
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Config {
+        pub(crate) openai_api_key: Cow<'static, str>,
+        pub surreal_db_path: Cow<'static, str>,
+    }
+
+    impl Config {
+        pub fn new<T1, T2>(openai_api_key: T1, surreal_db_path: T2) -> Self
+        where
+            T1: Into<Cow<'static, str>>,
+            T2: Into<Cow<'static, str>>,
+        {
+            Self {
+                openai_api_key: openai_api_key.into(),
+                surreal_db_path: surreal_db_path.into(),
+            }
+        }
+    }
+
+    fn get_config_path() -> Option<PathBuf> {
+        dirs::home_dir().map(|p| p.join(".config/cgpt/config.toml"))
+    }
+
+    pub async fn get_config() -> Option<Config> {
+        let path = get_config_path()?;
+        if fs::metadata(&path).await.is_ok() {
+            let content = fs::read(path).await.ok()?;
+            let content = String::from_utf8(content).ok()?;
+            task::spawn_blocking(move || toml::from_str::<Config>(&content).ok())
+                .await
+                .ok()
+                .flatten()
+        } else {
+            None
+        }
+    }
+
+    pub async fn save_config(config: Config) -> Result<PathBuf> {
+        let err_fn = || anyhow::anyhow!("Couldn't establish a config path!");
+        let path = get_config_path().ok_or_else(err_fn)?;
+        if fs::metadata(&path).await.is_err() {
+            let parent = path.parent().ok_or_else(err_fn)?;
+            fs::create_dir_all(parent).await?;
+        }
+        let toml = task::spawn_blocking(move || toml::to_string(&config)).await??;
+        let mut file = File::create(&path).await?;
+        file.write_all(toml.as_bytes()).await?;
+        Ok(path)
+    }
 }
