f6e4301f147b4f0e22000fbee8152ba8fabfaffb
Aslam Ahammed A <aslamplr@gmail.com>
2023-07-05 02:49:37 +0530

    Add rome lint and formatting

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,61 +1,59 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { listChat } from './api';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { listChat } from "./api";
 
 function App() {
-  const [texts, setTexts] = useState<string[]>([]);
+	const [texts, setTexts] = useState<string[]>([]);
 
-  useEffect(() => {
-    const fetchChat = async () => {
-      const chats = await listChat();
-      console.log(chats);
-    }
-    fetchChat();
-  }, []);
+	useEffect(() => {
+		const fetchChat = async () => {
+			const chats = await listChat();
+			console.log(chats);
+		};
+		fetchChat();
+	}, []);
 
-  const handleSendText = (text: string) => {
-    setTexts([...texts, text]);
-  };
-  return (
-    <>
-      {
-        texts.map((text, index) => {
-          return (
-            <div key={index}>
-              <p>{text}</p>
-            </div>
-          )
-        })
-      }
+	const handleSendText = (text: string) => {
+		setTexts([...texts, text]);
+	};
+	return (
+		<>
+			{texts.map((text) => {
+				return (
+					<div key={text}>
+						<p>{text}</p>
+					</div>
+				);
+			})}
 
-      <ChatInput sendText={handleSendText} />
-    </>
-  )
+			<ChatInput sendText={handleSendText} />
+		</>
+	);
 }
 
 interface ChatInputProps {
-  sendText: (text: string) => void
+	sendText: (text: string) => void;
 }
 
 function ChatInput(props: ChatInputProps) {
-  const [text, setText] = useState<string>("");
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter' && e.metaKey) {
-      console.log("Send message", text);
-      props.sendText(text);
-      setText("");
-    }
-  }
+	const [text, setText] = useState<string>("");
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (e.key === "Enter" && e.metaKey) {
+			console.log("Send message", text);
+			props.sendText(text);
+			setText("");
+		}
+	};
 
-  return (
-    <textarea
-      className="text-area footer"
-      placeholder="Type here..."
-      value={text}
-      onChange={(e) => setText(e.target.value)}
-      onKeyDown={(e) => handleKeyDown(e)}
-    ></textarea>
-  )
+	return (
+		<textarea
+			className="text-area footer"
+			placeholder="Type here..."
+			value={text}
+			onChange={(e) => setText(e.target.value)}
+			onKeyDown={(e) => handleKeyDown(e)}
+		/>
+	);
 }
 
-export default App
+export default App;
